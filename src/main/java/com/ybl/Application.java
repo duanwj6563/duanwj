c5c5fd94ed2925019ad167066732e3ce96598ad3
duanwj <15737976563@139.com>
2018-04-22 21:30:06 +0800

    增加健康监控

--- src/main/java/com/ybl/Application.java
+++ src/main/java/com/ybl/Application.java
@@ -1,11 +1,15 @@
 package com.ybl;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+/*@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector*/
 @EnableSwagger2
 @EnableJpaAuditing
 public class Application {
--- src/test/java/com/ybl/ApplicationTests.java
+++ src/test/java/com/ybl/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ybl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,16 +9,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class ApplicationTests {
     @Value("${duanwj.value}")
     private String name;
     @Value("${duanwj.int}")
-    private  int number;
+    private int number;
 
     @Test
     public void contextLoads() {
-        System.out.println("随机字符串"+name);
-        System.out.println("随机int"+number);
+        log.info("随机字符串{}" , name);
+        log.info("随机int{}" ,number);
+        log.error("日志级别error");
+        log.warn("日志级别warn");
+        log.info("日志级别info");
+        log.debug("日志级别debug");
     }
 
 }
